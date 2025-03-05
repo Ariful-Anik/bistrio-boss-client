435a8e5c7cc422437ea6683dabd44c4a01f9885c
Ariful Anik <arifulanik2021@gmail.com>
2025-03-05 16:18:26 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { router } from './Routes/Router.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'>
+    <div className='max-w-11/12 mx-auto'>
 
     <RouterProvider router={router} />
     </div>
